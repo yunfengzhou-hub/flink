51498880b1b36916ada38f4b741bcc81070436ee
yunfengzhou-hub <yuri.zhouyunfeng@outlook.com>
2023-07-05 09:17:20 +0800

    POC code for StreamRecord serialize optimization"

--- flink-core/src/main/java/org/apache/flink/api/common/ExecutionConfig.java
+++ flink-core/src/main/java/org/apache/flink/api/common/ExecutionConfig.java
@@ -761,6 +761,42 @@ public class ExecutionConfig implements Serializable, Archiveable<ArchivedExecut
         return configuration.get(PipelineOptions.OBJECT_REUSE);
     }
 
+    /**
+     * Enables timestamp attribute on records. See {@link PipelineOptions#RECORD_TIMESTAMP} for more
+     * details.
+     */
+    public ExecutionConfig enableRecordTimestamp() {
+        configuration.set(PipelineOptions.RECORD_TIMESTAMP, true);
+        return this;
+    }
+
+    /**
+     * Disables timestamp attribute on records. See {@link PipelineOptions#RECORD_TIMESTAMP} for
+     * more details.
+     */
+    public ExecutionConfig disableRecordTimestamp() {
+        configuration.set(PipelineOptions.RECORD_TIMESTAMP, false);
+        return this;
+    }
+
+    /**
+     * Returns whether record timestamps has been enabled or disabled. See {@link
+     * PipelineOptions#RECORD_TIMESTAMP} for more details.
+     */
+    public boolean isRecordTimestampEnabled() {
+        return configuration.get(PipelineOptions.RECORD_TIMESTAMP);
+    }
+
+    public ExecutionConfig setOperatorTimestamp(boolean operatorTimestamp) {
+        configuration.set(PipelineOptions.RECORD_TIMESTAMP, operatorTimestamp);
+        return this;
+    }
+
+    /** Returns whether object reuse has been enabled or disabled. @see #enableObjectReuse() */
+    public boolean isOperatorTimestampEnabled() {
+        return configuration.get(PipelineOptions.RECORD_TIMESTAMP);
+    }
+
     public GlobalJobParameters getGlobalJobParameters() {
         return globalJobParameters;
     }
@@ -1169,6 +1205,9 @@ public class ExecutionConfig implements Serializable, Archiveable<ArchivedExecut
                 .ifPresent(this::setMaxParallelism);
         configuration.getOptional(CoreOptions.DEFAULT_PARALLELISM).ifPresent(this::setParallelism);
         configuration.getOptional(PipelineOptions.OBJECT_REUSE).ifPresent(this::setObjectReuse);
+        configuration
+                .getOptional(PipelineOptions.RECORD_TIMESTAMP)
+                .ifPresent(this::setOperatorTimestamp);
         configuration
                 .getOptional(TaskManagerOptions.TASK_CANCELLATION_INTERVAL)
                 .ifPresent(this::setTaskCancellationInterval);
---
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/OperatorAttributes.java
@@ -0,0 +1,42 @@
+package org.apache.flink.streaming.api.operators;
+
+import org.apache.flink.annotation.PublicEvolving;
+
+/**
+ * OperatorAttributes element provides Job Manager with information that can be used to optimize the
+ * job performance.
+ */
+@PublicEvolving
+public class OperatorAttributes {
+    private final boolean isEmittingRecordsWithTimestamp;
+
+    private OperatorAttributes(boolean isEmittingRecordsWithTimestamp) {
+        this.isEmittingRecordsWithTimestamp = isEmittingRecordsWithTimestamp;
+    }
+
+    /**
+     * Returns true if the operator might assign additional timestamps to the output records. False
+     * if the operator only forward timestamps of existing input records to the output.
+     */
+    public boolean getIsEmittingRecordsWithTimestamp() {
+        return isEmittingRecordsWithTimestamp;
+    }
+
+    @PublicEvolving
+    public static class Builder {
+        private boolean isEmittingRecordsWithTimestamp;
+
+        public Builder() {
+            this.isEmittingRecordsWithTimestamp = false;
+        }
+
+        public Builder setIsEmittingRecordsWithTimestamp(boolean isEmittingRecordsWithTimestamp) {
+            this.isEmittingRecordsWithTimestamp = isEmittingRecordsWithTimestamp;
+            return this;
+        }
+
+        public OperatorAttributes build() {
+            return new OperatorAttributes(isEmittingRecordsWithTimestamp);
+        }
+    }
+}
