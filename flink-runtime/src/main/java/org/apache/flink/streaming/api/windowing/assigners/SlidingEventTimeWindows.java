08041f093192cafc29edd5f2855ee892dcd1be5e
yunfengzhou-hub <yuri.zhouyunfeng@outlook.com>
2024-09-25 09:06:32 +0800

    [FLINK-36355][runtime] Remove deprecated WindowAssigner#getDefaultTrigger

--- flink-runtime/src/main/java/org/apache/flink/streaming/api/windowing/assigners/SlidingEventTimeWindows.java
+++ flink-runtime/src/main/java/org/apache/flink/streaming/api/windowing/assigners/SlidingEventTimeWindows.java
@@ -21,7 +21,6 @@ package org.apache.flink.streaming.api.windowing.assigners;
 import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.EventTimeTrigger;
 import org.apache.flink.streaming.api.windowing.triggers.Trigger;
@@ -91,12 +90,6 @@ public class SlidingEventTimeWindows extends WindowAssigner<Object, TimeWindow> 
 
     public long getSlide() {
         return slide;
-    }
-
-    @Override
-    public Trigger<Object, TimeWindow> getDefaultTrigger(StreamExecutionEnvironment env) {
-        throw new UnsupportedOperationException(
-                "This method is deprecated and shouldn't be invoked. Please use getDefaultTrigger() instead.");
     }
 
     @Override
